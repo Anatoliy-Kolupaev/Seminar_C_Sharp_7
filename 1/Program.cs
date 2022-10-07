79414409026a5a0ff178d34a844a7edfa888a467
Anatoliy K <778966@mail.ru>
2022-10-07 15:38:22 +0500

    add 1

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: A = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+System.Console.Write("Введите число m: ");
+int m = int.Parse(System.Console.ReadLine());
+System.Console.Write("Введите число n: ");
+int n = int.Parse(System.Console.ReadLine());
+
+void Array (int m, int n)
+{
+    int [,] numbers = new int [m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            numbers [i, j] = i+j;
+            System.Console.Write( numbers [i,j]);
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+Array(m, n);
